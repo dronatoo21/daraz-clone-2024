d1f559098151247bae10525289389698a225e83b
dronatoo <mrdronatoo@gmail.com>
2024-03-14 17:16:39 -0700

    added logo in nav

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -7,17 +7,9 @@ const Navbar = () => {
         <NavLink><li>3</li></NavLink>
     </>
     return (
-        <div className="navbar bg-[#f65606] text-white py-6">
+        <div className="navbar bg-[#f65606] text-white py-5">
           <div className="navbar-start">
-            <div className="dropdown">
-              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-              </div>
-              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                {link}  
-              </ul>
-            </div>
-            <a className="btn btn-ghost text-xl">DARAZ</a>
+            <img className="h-12 w-32" src="//icms-image.slatic.net/images/ims-web/3ac46ea5-a376-41cc-981f-21fa690468c0.png" alt="logo" />
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">

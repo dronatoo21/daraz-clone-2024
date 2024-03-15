6186ec85974d2e962d9c6d7672fef88f0d5aa2f3
dronatoo <mrdronatoo@gmail.com>
2024-03-14 18:19:23 -0700

    worked on nav

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,23 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const link = <>
-        <NavLink><li>1</li></NavLink>
-        <NavLink><li>2</li></NavLink>
-        <NavLink><li>3</li></NavLink>
-    </>
     return (
-        <div className="navbar bg-[#f65606] text-white py-5">
-          <div className="navbar-start">
-            <img className="h-12 w-32" src="//icms-image.slatic.net/images/ims-web/3ac46ea5-a376-41cc-981f-21fa690468c0.png" alt="logo" />
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              {link}
-            </ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Button</a>
+        <div className="navbar flex bg-[#f65606] text-white pt-7">
+          <div className="container mx-auto">
+            <div className="absolute top-0 text-sm">
+              <div className="flex gap-3">
+                <Link to="#"><p className="">Become a Seller</p></Link>
+                <Link to="#"><p className="">Daraz Donates</p></Link>
+                <div className="dropdown dropdown-hover text-black">
+                  <div tabIndex={0} role="button" className="text-white">Help & Support</div>
+                  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex flex-row justify-between">
+              <div>
+                <img className="h-12 w-32" src="//icms-image.slatic.net/images/ims-web/3ac46ea5-a376-41cc-981f-21fa690468c0.png" alt="logo" />
+              </div>
+              <div>
+                <h1>-----------------------</h1>
+              </div>
+              <div>
+                <a className="btn">Button</a>
+              </div>
+            </div>
           </div>
         </div>
     );

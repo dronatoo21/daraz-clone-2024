501a1549aaa2bb3e4c626a67d876a3ebcefd1076
dronatoo <mrdronatoo@gmail.com>
2024-03-27 14:28:11 -0700

    completed below banner

--- src/Pages/Home/Banner/BannerBelow.jsx
+++ src/Pages/Home/Banner/BannerBelow.jsx
@@ -1,8 +1,7 @@
 const BannerBelow = () => {
     return (
         <div>
-            <div className="h-[200px] border-2" style={{backgroundImage: "url(https://icms-image.slatic.net/images/ims-web/9264858a-dbb6-4886-8195-15a0d744df7c.gif)"}}>
-
+            <div className="h-[200px] border-2" style={{backgroundImage: "url(https://icms-image.slatic.net/images/ims-web/9264858a-dbb6-4886-8195-15a0d744df7c.gif)", backgroundPositionX: "-170px"}}>
             </div>
         </div>
     );

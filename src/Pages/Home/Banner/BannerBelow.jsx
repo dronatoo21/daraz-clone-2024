2aa5a9edbba18ae67e1f535d8db60731a8c03eeb
dronatoo <mrdronatoo@gmail.com>
2024-03-27 14:24:04 -0700

    working on below banner

---
+++ src/Pages/Home/Banner/BannerBelow.jsx
@@ -0,0 +1,11 @@
+const BannerBelow = () => {
+    return (
+        <div>
+            <div className="h-[200px] border-2" style={{backgroundImage: "url(https://icms-image.slatic.net/images/ims-web/9264858a-dbb6-4886-8195-15a0d744df7c.gif)"}}>
+
+            </div>
+        </div>
+    );
+};
+
+export default BannerBelow;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import BannerBelow from "../Banner/BannerBelow";
 
 const Home = () => {
     return (
         <div className="container mx-auto">
             <Banner/>
+            <BannerBelow/>
         </div>
     );
 };

e846023708e39648a0dfa8f664dc83383d35780e
dronatoo <mrdronatoo@gmail.com>
2024-03-30 13:16:20 -0700

    added flash sale

---
+++ src/Pages/Home/FlashSale/FlashSale.jsx
@@ -0,0 +1,17 @@
+const FlashSale = () => {
+    return (
+        <div className="mt-20">
+            <div>
+                <div className="flex justify-between"> 
+                    <h1 className="text-orange-500">On Sale Now</h1>
+                    <button className="btn text-orange-600">Shop More</button>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FlashSale;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import BannerBelow from "../Banner/BannerBelow";
+import FlashSale from "../FlashSale/FlashSale";
 
 const Home = () => {
     return (
         <div className="container mx-auto">
             <Banner/>
             <BannerBelow/>
+            <FlashSale/>
         </div>
     );
 };

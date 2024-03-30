923e36a2a19d172ea5aa22a8cb3a7b56c0b7055a
dronatoo <mrdronatoo@gmail.com>
2024-03-30 13:36:33 -0700

    added card on flash sale

--- src/Pages/Home/FlashSale/FlashSale.jsx
+++ src/Pages/Home/FlashSale/FlashSale.jsx
@@ -2,12 +2,22 @@ const FlashSale = () => {
     return (
         <div className="mt-20">
             <div>
-                <div className="flex justify-between"> 
+                <div className="flex justify-between px-10 py-3 items-center bg-white"> 
                     <h1 className="text-orange-500">On Sale Now</h1>
-                    <button className="btn text-orange-600">Shop More</button>
+                    <button className="btn bg-white text-orange-600">Shop More</button>
                 </div>
-                <div>
-
+                <hr />
+                <div className="bg-white p-5">
+                <div className="card w-[250px] card-compact bg-base-100 shadow-xl">
+                  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                  <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                  </div>
+                </div>
                 </div>
             </div>
         </div>
